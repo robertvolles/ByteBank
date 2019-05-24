32c75578294bebe0f21b6c961fa8e2e7aa5d957a
Estagiario 004 <stag004@benner.com.br>
2019-05-24 10:21:06 -0300

    Atribuindo valores para objetos da classe

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine(contaDaGabriela.agencia);
+            Console.WriteLine(contaDaGabriela.numero);
+            Console.WriteLine(contaDaGabriela.saldo);
 
 
             Console.ReadLine();
